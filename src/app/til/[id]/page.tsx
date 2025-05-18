e7de9dea2e3911b2916fda359e708e49e6218562
Otto Lenz <otto.lenz@hotmail.com>
2025-05-18 11:18:44 +0100

    🔥 Force rebuild by removing type constraint on params

--- src/app/til/[id]/page.tsx
+++ src/app/til/[id]/page.tsx
@@ -1,6 +1,7 @@
 import CommentSection from "../../../components/CommentSection";
 
-export default async function TilPage({ params }: any) {
+export default async function TilPage({ params }: any) { 
+  // 🔥 Forced rebuild attempt: removing type issues
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/get-til-by-id?id=${params.id}`,
     { cache: "no-store" }
