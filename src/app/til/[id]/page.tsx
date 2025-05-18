0371f759bbd38552141fd98341a5176929220179
Otto Lenz <otto.lenz@hotmail.com>
2025-05-18 18:23:56 +0100

    Chore: removed unused useRouter import and eslint directive

--- src/app/til/[id]/page.tsx
+++ src/app/til/[id]/page.tsx
@@ -1,13 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useRouter } from "next/router";
 import CommentSection from "../../../components/CommentSection";
 
 export default function TilPage({ params }: { params: { id: string } }) {
   const { id } = params;
 
   return (
-    <div>
-      {/* TIL content here */}
+    <div className="p-4">
+      {/* Your TIL content here */}
       <CommentSection tilId={id} />
     </div>
   );
