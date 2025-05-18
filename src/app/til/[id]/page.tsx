b885d1746124751c38e04379159adc4e0cdd447e
Otto Lenz <otto.lenz@hotmail.com>
2025-05-18 18:27:48 +0100

    Fix: cleaned up duplicated return block in /til/[id]/page.tsx

--- src/app/til/[id]/page.tsx
+++ src/app/til/[id]/page.tsx
@@ -1,11 +1,16 @@
 import CommentSection from "../../../components/CommentSection";
 
-export default function TilPage({ params }: { params: { id: string } }) {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export default function TilPage({ params }: Props) {
   const { id } = params;
 
   return (
     <div className="p-4">
-      {/* Your TIL content here */}
       <CommentSection tilId={id} />
     </div>
   );
