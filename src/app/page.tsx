08710186be7483b91d3fbb28c9ea9e5e4f7dc9e3
Otto Lenz <otto.lenz@hotmail.com>
2025-05-25 11:17:40 +0100

    Add homepage to fix 404

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// src/app/page.tsx
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Welcome to WHILT 🧠</h1>
+      <p>This is your homepage. Time to learn!</p>
+    </main>
+  );
+}
