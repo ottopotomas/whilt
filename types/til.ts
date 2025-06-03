7ed5a2a30c4d959fa6b645ac866c85889b17f52c
Otto Lenz <otto.lenz@hotmail.com>
2025-06-03 09:44:51 +0100

    Add export for TIL type to fix module import error

---
+++ types/til.ts
@@ -0,0 +1,11 @@
+export type TIL = {
+  id: string;
+  user: string;
+  content: string;
+  category: string;
+  imageUrl?: string;
+  sourceUrl?: string;
+  avatarUrl?: string;
+  addedByCount?: number;
+  created_at: string;
+};
